ec5572697ef20e26b5d29303d7a654cc56c89619
mouhcine1231 <59617266+mouhcine1231@users.noreply.github.com>
2022-11-20 14:44:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ new userAccount(client, Discord).leveling({
     time: 10000, 
     type: 'eng' 
 });
-client.login("token");
+client.login("ODU1NDQxNzAwMzgwOTk5Njkw.GYR_zs.tfJFRxYJvyo8H1Bdq1DuwNjKjdS1UTwFc6D0sw");
 //https://ra3dstudio.com CopyRight Codes
