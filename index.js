65ef45acc6d6006b6afa125958f16612d6818b19
mouhcine1231 <59617266+mouhcine1231@users.noreply.github.com>
2022-11-20 14:36:25 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+//Code to raise the level of robot accounts | كود ل رفع لفل حسابات بروبوت
+const Discord = require("discord.js-selfbot-v13"); 
+const client = new Discord.Client({
+    intents: [Discord.Intents.FLAGS.GUILDS]
+});
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+const {userAccount} = require("sphinx-run");
+new userAccount(client, Discord).leveling({
+    channel: '1029022367437037590',
+    randomLetters: false, 
+    time: 10000, 
+    type: 'eng' 
+});
+client.login("ODU1NDQxNzAwMzgwOTk5Njkw.GYR_zs.tfJFRxYJvyo8H1Bdq1DuwNjKjdS1UTwFc6D0sw");
+//https://ra3dstudio.com CopyRight Codes
